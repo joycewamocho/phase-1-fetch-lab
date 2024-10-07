c5774bd635a129c8c3a79d9323ccf73d62581206
joyce wamocho <joywamo@gmail.com>
2024-10-07 17:48:17 +0300

    fetch lab

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((res) =>res.json())
+  .then( data => {
+    renderBooks(data)
+    return data
+  })
 }
 
 function renderBooks(books) {
